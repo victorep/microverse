ffcbb34d8dc48447d89358af924b4b56ffc5a9c9
victor <victor.epure@gmail.com>
2017-09-25 21:17:43 +0300

    25/09-1 victor

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/event', (req, res) => {
-  res.send('The event ID is the ' + events[0].title)
-});
+app.use(bodyParser.json());
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
@@ -13,6 +16,31 @@ app.post('/', function (req, res) {
   res.send('Got a POST request')
 });
 
+//Event endpoints
+app.get('/events', (req,res) =>{
+	let output="";
+	//for i =0 to size (events)
+	//	show "Event " + i + ": " + events[i].title;
+	output="<ul>";
+	for (var i in events) {    // don't actually do this
+  		output += '<li><a href="/events/'+i+'">Event ' + i+ ': ' + events[i].title +'</a></li>\n';
+	}
+	output+="</ul>";
+	res.send(output);
+});
+
+app.get('/events/:id', (req, res) => {
+  res.send("The event title for event "+req.params.id+" is the " + events[req.params.id].title)
+});
+
+app.post('/events', (req, res) => {
+	console.log(req.body.title);
+	//console.log('This would create a new object with title ' + req.body.title + ', definition ' + req.body.description + ' and date ' +req.body.date);
+  res.send('')
+})
+
+
+//event definition
 let event1 = {
 	id: 1, 
   title:"Tom",
